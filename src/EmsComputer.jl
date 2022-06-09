65b9fa0c2b89ef18b5e882247d8ca04ea843d891
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-08 23:27:07 -0500

    updated env/code

--- src/EmsComputer.jl
+++ src/EmsComputer.jl
@@ -3,12 +3,34 @@ function main(routes::Vector{Route})
     server.start()
 end
 
+logo = img("logo", src = "https://i.ibb.co/NZhMDn8/animated.gif")
+lfade = Animation("fade_left")
+lfade[:from] = "opacity" => "%0"
+lfade[:to] = "opacity" => "%100"
+logo_style = Style("img")
+animate!(logo_style, lfade)
+style!(logo, logo_style)
+heading_style = Style("h1")
+heading_style["color"] = "pink"
+header_css = [lfade, logo_style, heading_style]
+header = Toolips.div("header", [logo])
+header[:align] = "center"
 
 hello_world = route("/") do c
-    write!(c, p("hello", text = "hello world!"))
+    for s in header_css
+        write!(c, s)
+    end
+    write!(c, header)
+    write!(c, "<h1>")
+    for char in "welcome to em's computer"
+        write!(c, string(char))
+        sleep(.1)
+    end
+    write!(c, "</h1>")
 end
-fourofour = route("404", p("404", text = "404, not found!"))
+
+fourofour = route("404") do c
+    write!(c, p("404", text = "404, not found!"))
+end
 rs = routes(hello_world, fourofour)
 main(rs)
-
-        
