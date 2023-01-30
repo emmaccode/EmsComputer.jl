c1c92981715e5c3b654493c7539cce91ef58d371
emmy <52672675+emmettgb@users.noreply.github.com>
2023-01-30 04:10:25 -0600

    tweaked posts, transition

--- src/Pages.jl
+++ src/Pages.jl
@@ -1,12 +1,56 @@
+function build_main(c::Connection, cm::ComponentModifier, bod::Component{:body})
+    style!(cm, "maindiv", "height" => 0percent, "opacity" => "0percent")
+    style!(cm, "mainbody", "background-color" => "#36454F", "transition" => "2.5s")
+    remove!(cm, "emsfooter")
+    app_panel = section("emsapps")
+    searchbox_div::Component{:div} = div("searchb_c")
+    style!(searchbox_div, "background-color" => "#888C8D")
+    searchinput::Component{:div} = ToolipsDefaults.textdiv("searchinp")
+    style!(searchinput, "color" => "white", "background-color" => "#888C8D")
+    push!(searchbox_div, searchinput)
+    style!(app_panel, "background-color" => "#320064", "border-color" => "#141414")
+    push!(app_panel, searchbox_div)
+    for post_dir in readdir("public/content/posts")
+        psh::Post = Post("public/content/posts/" * post_dir)
+        postbody = section("postbody")
+        on(c, postbody, "click") do cm2::ComponentModifier
+            style!(cm2, "mainbody", "background-color" => "#141414")
+            style!(cm2, "maindiv", "height" => 0percent, "opacity" => 0percent)
+            style!(cm2, "emseyes", "transform" => "translateX(-300%)")
+            remove!(cm2, "betaemblem")
+            next!(c, bod, cm2) do cm3::ComponentModifier
+                
+            end
+        end
+        push!(postbody, img("$(psh.title)-i", src = psh.img, width = 300),
+         h("$(psh.title)-t", 2, text = psh.title),
+        h("$(psh.title)-st", 4, text = psh.sub))
+        push!(app_panel, postbody)
+    end
+    next!(c, bod, cm) do cm2::ComponentModifier
+        set_children!(cm2, "maindiv", [app_panel])
+        cm2["emseyes"] = "src" => "/images/animated.gif"
+        style!(cm2, "maindiv", "background" => "transparent", "height" => 100percent,
+        "width" => 50percent, "opacity" => 100percent, "margin-left" => 25percent,
+        "margin-top" => 15px)
+        style!(cm2, "emseyes", "transform" => "scale(.7)", "transition" => 2seconds,
+        "margin-top" => 5px)
+        style!(cm2, "mainbody", "overflow-y" => "scroll")
+    end
+end
+
+function create_styles()
+    stylsheet::Component{:sheet} = ToolipsDefaults.sheet("emsstyles")
+    stylsheet[:children]["h2"]["color"] = "white"
+    stylsheet[:children]["h4"]["color"] = "lightblue"
+    stylsheet[:children]["section"]["border-color"] = "gray"
+    stylsheet::Component{:sheet}
+end
 
 function home(c::Connection)
     uri::String = "/images/animated.gif"
     write!(c, title("thetitle", text = "em's computer !"))
-    stylsheet = ToolipsDefaults.sheet("emsstyles")
-    stylsheet[:children]["h2"]["color"] = "white"
-    stylsheet[:children]["h4"]["color"] = "lightblue"
-    stylsheet[:children]["section"]["border-color"] = "gray"
-    write!(c, stylsheet)
+    write!(c, create_styles())
     logobg = div("logobg", align = "center")
     style!(logobg, "background" => "transparent", "margin-top" => 5percent,
     "opacity" => 0percent, "transform" => "translateX(20%)", "transition" => 2seconds,
@@ -16,7 +60,8 @@ function home(c::Connection)
     style!(betaemblem, "color" => "white", "transform" => "translateX(5%)")
     push!(logobg, header, betaemblem)
     body = Component("mainbody", "body")
-    style!(body, "background-color" => "#D6CBDA", "overflow" => "hidden")
+    style!(body, "background-color" => "#D6CBDA", "overflow" => "hidden",
+    "transition" => 8seconds)
     main = divider("maindiv", align = "left")
     style!(main, "background-color" => "#36454F", "margin-top" => 2percent,
     "width" => 20percent, "margin-left" => 40percent, "position" => "absolute",
@@ -62,30 +107,7 @@ function home(c::Connection)
     "width" => 20percent, "margin-top" => 15px, "margin-left" => 20percent)
     style!(pwdlabel, "color" => "lightpink")
     on(c, skipbutton, "click") do cm::ComponentModifier
-        style!(cm, main, "height" => 0percent, "opacity" => "0percent")
-        style!(cm, body, "background-color" => "#36454F", "transition" => "2.5s")
-        remove!(cm, emsfooter)
-        app_panel = section("emsapps")
-        style!(app_panel, "background-color" => "#320064", "border-color" => "#141414")
-        for post_dir in readdir("public/content/posts")
-            psh::Post = Post("public/content/posts/" * post_dir)
-            postbody = section("postbody")
-            push!(postbody, img("$(psh.title)-i", src = psh.img, width = 300),
-             h("$(psh.title)-t", 2, text = psh.title),
-            h("$(psh.title)-st", 4, text = psh.sub))
-            push!(app_panel, postbody)
-        end
-        next!(c, main, cm) do cm2::ComponentModifier
-            sleep(2)
-            set_children!(cm2, main, [app_panel])
-            cm2[header] = "src" => uri
-            style!(cm2, main, "background" => "transparent", "height" => 100percent,
-            "width" => 50percent, "opacity" => 100percent, "margin-left" => 25percent,
-            "margin-top" => 15px)
-            style!(cm2, header, "transform" => "scale(.7)", "transition" => 2seconds,
-            "margin-top" => 5px)
-            style!(cm2, body, "overflow-y" => "scroll")
-        end
+        build_main(c, cm, body)
     end
     push!(loginstuff, loginheading, unamelabel, unamebox, br(), pwdlabel, mainpwdbox,
     signupbutton, skipbutton, loginbutton)
