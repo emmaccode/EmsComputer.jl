22db56e8fdae0b1000b49a12176310f54a5c196e
emmy <52672675+emmaccode@users.noreply.github.com>
2025-02-21 11:06:01 -0700

    completed first page (posts) (mostly)

--- src/EmsComputer.jl
+++ src/EmsComputer.jl
@@ -10,28 +10,8 @@ include("Pages.jl")
 include("Styles.jl")
 SESSION = Session(["/"])
 include("desktop.jl")
-mutable struct Post <: Servable
-    ID::String
-    readcount::Int64
-    stars::Int64
-    title::String
-    sub::String
-    img::String
-    tags::Vector{String}
-    uri::String
-    function Post(uri::String)
-        p = read(uri, String)
-        metas = findfirst("```meta", p)
-        imgs = findfirst("```img", p)
-        metar = maximum(metas) + 1:findnext("```", p, metas[2])[1] - 1
-        imgr = maximum(imgs) + 1:findnext("```", p, imgs[2])[1] - 1
-        imgdata = replace(p[imgr], "\n" => "")
-        metainfo = TOML.parse(p[metar])
-        ID = ToolipsSession.gen_ref(16)
-        new(ID, metainfo["readcount"], metainfo["stars"], metainfo["title"],
-        metainfo["sub"], imgdata, Vector{String}(split(metainfo["tags"], ",")), uri)
-    end
-end
+include("posts.jl")
+
 
 fourofour = route("404") do c
     header = make_header(c, "/images/animated.gif")
--- src/posts.jl
+++ src/posts.jl
@@ -1,14 +1,47 @@
+mutable struct Post <: Servable
+    ID::String
+    readcount::Int64
+    stars::Int64
+    title::String
+    sub::String
+    img::String
+    tags::Vector{String}
+    uri::String
+    function Post(uri::String)
+        p = read(uri, String)
+        metas = findfirst("```meta", p)
+        imgs = findfirst("```img", p)
+        metar = maximum(metas) + 1:findnext("```", p, metas[2])[1] - 1
+        imgr = maximum(imgs) + 1:findnext("```", p, imgs[2])[1] - 1
+        imgdata = replace(p[imgr], "\n" => "")
+        metainfo = TOML.parse(p[metar])
+        ID = ToolipsSession.gen_ref(16)
+        new(ID, metainfo["readcount"], metainfo["stars"], metainfo["title"],
+        metainfo["sub"], imgdata, Vector{String}(split(metainfo["tags"], ",")), uri)
+    end
+end
+
+posts_main = div("posts-main", align = "left")
+style!(posts_main, "padding" => 50px, "overflow-x" => "show", "overflow-y" => "scroll")
+for post_dir in readdir("public/content/posts")
+    psh::Post = Post("public/content/posts/" * post_dir)
+    postbody = section("postbody")
+    push!(postbody, img("$(psh.title)-i", src = psh.img, width = 300),
+    h3("$(psh.title)-t", text = psh.title),
+    h4("$(psh.title)-st", text = psh.sub))
+    push!(posts_main, postbody)
+end
+
 function make_windowmenu(c::AbstractConnection, app::ColorPagesApp{:posts})
-    bar = div("posts-menu", align = "right", expanded = 1)
+    bar = div("posts-menu", align = "right", expanded = 1, children = [posts_main])
     on(c, bar, "click") do cm::ComponentModifier
-        if cm["colorpages-menu"]["expanded"] == "0"
-            style!(cm, "colorpages-menu", "width" => 100percent, "padding" => 20px)
-            set_children!(cm, "colorpages-menu", 
-            Vector{AbstractComponent}([close_app_button, (make_app_preview(c, app) for app in APPS) ...]))
-            cm["colorpages-menu"] = "expanded" => "1"
+        if cm["posts-menu"]["expanded"] == "0"
+            style!(cm, "posts-menu", "width" => 100percent)
+
         end
     end
     style!(bar, "background-color" => app.color, "height" => 85percent, 
-    "width" => 0percent, "transition" => 700ms, "display" => "inline-block", "display" => "none")
+    "width" => 0percent, "transition" => 700ms, "display" => "inline-block", "display" => "none", 
+    "overflow" => "hidden")
     bar
 end
